6cfe3307acbee94510475f0db0814745d283a32d
dev <sheyiadekoya@gmail.com>
2018-11-11 11:21:36 +0100

    remove react snap postbuild

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
+import { Helmet } from 'react-helmet';
+
 import HomePage from './pages/HomePage';
 import ProjectsPage from './pages/ProjectsPage';
 import ResumePage from './pages/ResumePage';
@@ -12,12 +14,18 @@ class App extends Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route path="/projects" component={ProjectsPage} />
-          <Route path="/resume" component={ResumePage} />
-          <Route component={HomePage} />
-        </Switch>
+        <div>
+          <Helmet>
+            <title>Seyi Adekoya - Full Stack Engineer</title>
+          </Helmet>
+
+          <Switch>
+            <Route exact path="/" component={HomePage} />
+            <Route path="/projects" component={ProjectsPage} />
+            <Route path="/resume" component={ResumePage} />
+            <Route component={HomePage} />
+          </Switch>
+        </div>
       </Router>
     );
   }
